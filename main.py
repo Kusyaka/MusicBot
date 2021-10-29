ecf93667e1986f42cb51b3a776d3ba821749592c
Kusyaka <46048979+Kusyaka@users.noreply.github.com>
2021-10-29 14:39:12 +0300

    я люблю пэнисы

--- main.py
+++ main.py
@@ -96,15 +96,19 @@ class CommandsHandler(commands.Cog):
 
         self._YTDL_OPTIONS = {
             'format': 'bestaudio/best',
+            'extractaudio': True,
+            'audioformat': 'mp3',
+            'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+            'restrictfilenames': True,
             'noplaylist': True,
-            'nocheckcertificate': False,
+            'nocheckcertificate': True,
             'ignoreerrors': False,
             'logtostderr': False,
             'quiet': True,
             'no_warnings': True,
-            'default_search': 'auto',
+            'default_search': 'ytsearch',
             'source_address': '0.0.0.0',
-            'skip_download': True,
+            'simulate': True
         }
 
         self._FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 '
@@ -120,12 +124,17 @@ class CommandsHandler(commands.Cog):
     @commands.command(aliases=['sk'])
     async def skip(self, ctx):
         curr_guild = ctx.guild.id
+        try:
+            self.is_live[curr_guild] = False
+        except:
+            pass
         self._is_playing[curr_guild] = False
         (await self.get_voice_client(ctx)).stop()
 
-    @commands.command
+    @commands.command(aliases=['leave'])
     async def stop(self, ctx):
         curr_guild = ctx.guild.id
+
         try:
             self.is_live[curr_guild] = False
         except:
@@ -133,18 +142,12 @@ class CommandsHandler(commands.Cog):
         self._music_queue[curr_guild] = []
         (await self.get_voice_client(ctx)).stop()
         self._is_playing[curr_guild] = False
-
+        await (await self.get_voice_client(ctx)).disconnect()
+        del self._vc[curr_guild]
 
     @commands.command()
     async def join(self, ctx):
         await ctx.author.voice.channel.connect()
-
-    @commands.command()
-    async def leave(self, ctx):
-        curr_guild = ctx.guild.id
-        await self.stop(ctx)
-        self._vc[curr_guild].disconnect()
-        del self._vc[curr_guild]
 
     @commands.command(aliases=['auto'])
     async def autoplay(self, ctx):
@@ -172,10 +175,19 @@ class CommandsHandler(commands.Cog):
             self._autoplay[curr_guild] = False
         song_type = self.identify_url(query)
         track = None
+        wait_msg = discord.Embed(title="Идёт поиск...", description="Может занять некоторое время", color=0x46c077)
+        wait_msg = await ctx.send(embed=wait_msg)
         if song_type == Sites.Unknown:
             track = self.search_yt(ctx, query)
         elif song_type == Sites.YouTube:
             track = self.get_ytdl(ctx, query)
+
+        await wait_msg.delete()
+        if isinstance(track, bool):
+            embed = discord.Embed(title="Не найдено", description=query, color=0xff0000)
+            msg = await ctx.send(embed=embed)
+            await msg.delete(delay=5)
+            return
 
         track.add_data(type=song_type)
 
@@ -189,19 +201,23 @@ class CommandsHandler(commands.Cog):
             asyncio.get_event_loop().create_task(self._play_queue(ctx))
 
     def search_yt(self, ctx, item):
-        data = self.ytdl.extract_info(f"ytsearch:{item}", download=False)['entries'][0]
-
-        return Track(title=data['title'], url="https://www.youtube.com/watch?v=" + data['id'],
-                     thumbnail=data['thumbnail'], duration=data['duration'], live=data['is_live'])
-
+        try:
+            data = self.ytdl.extract_info(f"ytsearch:{item}", download=False)['entries'][0]
+            self._last_url[ctx.guild.id] = data['webpage_url']
+            return Track(title=data['title'], url=data['webpage_url'],
+                         thumbnail=data['thumbnail'], duration=data['duration'], live=data['is_live'])
+        except:
+            return False
 
     def get_ytdl(self, ctx, url):
-        data = self.ytdl.extract_info(url, download=False)
-        self._last_url[ctx.guild.id] = url
+        try:
+            data = self.ytdl.extract_info(url, download=False)
+            self._last_url[ctx.guild.id] = url
 
-        return Track(title=data['title'], url=data['webpage_url'],
-                     thumbnail=data['thumbnail'], duration=data['duration'], live=data['is_live'])
-
+            return Track(title=data['title'], url=data['webpage_url'],
+                         thumbnail=data['thumbnail'], duration=data['duration'], live=data['is_live'])
+        except:
+            return False
 
     def identify_url(self, url):
         if url is None:
@@ -249,25 +265,37 @@ class CommandsHandler(commands.Cog):
                 await asyncio.sleep(5)
 
             if self._autoplay[curr_guild]:
+                wait_msg = discord.Embed(title="Идёт поиск...", description="Может занять некоторое время",
+                                         color=0x46c077)
+                wait_msg = await ctx.send(embed=wait_msg)
                 self.__autoplay(ctx)
+                await wait_msg.delete()
 
     async def _play_song(self, ctx):
         curr_guild = ctx.guild.id
         curr_vc = await self.get_voice_client(ctx)
+        curr_track = self._music_queue[curr_guild][0]
         m_url = self._music_queue[curr_guild][0].get_media_url(self.ytdl)
         print(self._music_queue[curr_guild][0].url)
+
+        embed = discord.Embed(title=curr_track.title, url=curr_track.url,
+                              description="Сейчас играет", color=0x46c077)
+        embed.set_thumbnail(url=curr_track.thumbnail)
+        await ctx.send(embed=embed)
 
         curr_vc.stop()
         self._is_playing[curr_guild] = True
 
         def end():
             self._is_playing[curr_guild] = False
+
         def end_s():
             self.is_live[curr_guild] = False
+
         if curr_guild not in self.is_live:
             self.is_live[curr_guild] = False
         self.is_live[curr_guild] = self._music_queue[curr_guild][0].live
-        curr_track = self._music_queue[curr_guild][0]
+
         self._music_queue[curr_guild].pop(0)
         if self.is_live[curr_guild]:
             while self.is_live[curr_guild]:
@@ -294,11 +322,11 @@ class CommandsHandler(commands.Cog):
         options.add_argument("--headless")
         driver = webdriver.Firefox(firefox_options=options, log_path=os.devnull)
         track = None
-
         while True:
             driver.get(self._last_url[curr_guild])
             time.sleep(2)
-            elems = driver.execute_script('return document.getElementsByClassName("yt-simple-endpoint inline-block style-scope ytd-thumbnail")')[1:]
+            elems = driver.execute_script('return document.getElementsByClassName("yt-simple-endpoint inline-block style-scope ytd-thumbnail")')
+            elems = elems[1: 5 if len(elems) >= 5 else len(elems)-1]
             track = self.get_ytdl(ctx, random.choice(elems).get_attribute('href'))
             if type(track) in (bool, NoneType):
                 continue
